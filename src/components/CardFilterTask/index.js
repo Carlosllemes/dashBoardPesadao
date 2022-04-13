28f9e24580346c4b5f67bdb56c1400ab362f1697
Carlos Lemes <carlos.lemes@doutoresdaweb.com.br>
2022-04-12 21:47:02 -0300

    regexfilter

--- src/components/CardFilterTask/index.js
+++ src/components/CardFilterTask/index.js
@@ -4,97 +4,91 @@ import DropFilterCard from "../DropFilterCard/";
 import Api from "../../services/api";
 import "./style.scss";
 
-export default function CardFilterTask (props){
-
+export default function CardFilterTask(props) {
   const [tasks, setTask] = useState([]);
-  const [filterTask, setFilterTask] = useState([])
-
-  useEffect(()=>{
-    console.log('FDP DE EFFECT')
-  }, [filterTask])
+  const [filterTask, setFilterTask] = useState([]);
+  const [alteration, setAlteration] = useState([]);
 
   useEffect(() => {
-    const api = new Api()
+    const api = new Api();
     async function fetchData() {
-      var response = await api.getRquest('tasks?user_id=desenvolvimento&is_closed=true&limit=10')
+      var response = await api.getRquest(
+        "tasks?user_id=desenvolvimento&is_closed=true&limit=30"
+      );
       setTask(response);
+      setAlteration(0);
+      setFilterTask("Hoje");
     }
-    fetchData()
+    fetchData();
   }, []);
 
-
-   function amountTasks(data, intervalDate) {
+  function amountTasks(data, intervalDate) {
     try {
       let today = new Date().toLocaleDateString("pt-BR");
-      let contador = 1;
+      let count = 1;
       data.map((data) => {
-        let dateTask = new Date(data.close_date).getTime();
-        let dateFinish = timeTask(today, intervalDate);
+        let dateCloseTask = new Date(data.close_date).getTime();
+        let dateFilter = timeTask(today, intervalDate).getTime();
         if (getTypeTask(data.type_name)) {
-          if (dateTask > dateFinish) {
-            contador++;
-            console.log("contador "+contador)
-          }
+          if (dateCloseTask > dateFilter) setAlteration(count++);
         }
-        return contador;
+        return count;
       });
     } catch (err) {
       console.log(err);
     }
   }
 
-
-
   function timeTask(date, intervalDate) {
     var date = new Date();
     date.setDate(date.getDate() - intervalDate);
-    console.log("data atual - "+intervalDate+ "dias "+date)
     return date;
   }
 
   function getTypeTask(task) {
-    if (
-      task === "Produção - Alt Nv. 2" ||
-      task === "Produção - Alt Nv. 1" ||
-      task === "Produção - Alt Nv. 3" ||
-      task === "Produção - Alt Nv. 4"
-    ) {
-      return true;
-    }
+    const regex = /(.Alt*)/;
+    let result = regex.test(task);
+    if (result) return true;
   }
 
+  return (
+    <CardGroup className="info-card">
+      <Card.Body>
+        <Container className="info-card--header">
+          <Card.Text as="h5">
+            Alteracoes <span>| {filterTask}</span>
+          </Card.Text>
 
- 
-    return (
-      <CardGroup className="info-card">
-        <Card.Body>
-          <Container className="info-card--header">
-            <Card.Text as="h5">
-              Alterações <span>| {filterTask}</span>
-            </Card.Text>
-
-            <DropFilterCard
-              amountDay={() => {amountTasks(tasks, 1);setFilterTask("Hoje")}}
-              amountWeek={() => {amountTasks(tasks, 7);setFilterTask("Semana")}}
-              amountMonth={() => {amountTasks(tasks, 30);setFilterTask("Mes")}}
-            />
+          <DropFilterCard
+            amountDay={() => {
+              amountTasks(tasks, 1);
+              setFilterTask("Hoje");
+            }}
+            amountWeek={() => {
+              amountTasks(tasks, 7);
+              setFilterTask("Semana");
+            }}
+            amountMonth={() => {
+              amountTasks(tasks, 30);
+              setFilterTask("Mes");
+            }}
+          />
+        </Container>
+        <Container className="info-card--middle">
+          <Card.Img
+            className="rounded-circle d-flex align-items-center justify-content-center"
+            src="icons/svg/sites-alteration.svg"
+          />
+          <Container>
+            <h6>{alteration}</h6>
+            <p>
+              <span className="text-success small pt-1 fw-bold">
+                fechados
+              </span>
+            </p>
           </Container>
-          <Container className="info-card--middle">
-            <Card.Img
-              className="rounded-circle d-flex align-items-center justify-content-center"
-              src="icons/svg/sites-alteration.svg"
-            />
-            <Container>
-              <h6>{props.nome}</h6>
-              <p>
-                <span className="text-success small pt-1 fw-bold">
-                </span>
-                  {tasks.length}
-                <span className="text-muted small pt-2 ps-1">increase</span>
-              </p>
-            </Container>
-          </Container>
-        </Card.Body>
-      </CardGroup>
-    );
+        </Container>
+      </Card.Body>
+    </CardGroup>
+  );
 }
