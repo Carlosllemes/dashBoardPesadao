d5fff4cd17683cf2163eb5734c29aed1a8425967
Carlos Lemes <carlos.lemes@doutoresdaweb.com.br>
2022-04-15 12:58:01 -0300

    remove-fetch-cardFilterTask

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "../src/main.scss";
 import Routes from "./model/routes/Routes";
 import CardFilterTask from "./components/CardFilterTask";
 import { Container } from "react-bootstrap";
+import Api from "./services/api";
 
 export default function App() {
+  const api = new Api();
+  const url = "tasks?user_id=desenvolvimento&";
+
+  const [tasks, setTask] = useState([]);
+  const [openTasks, setOpenTasks] = useState([]);
+  const [progress, setProgress] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    // fetch all tasks
+    const closed_task = await api.getRquest(url + "is_closed=true&limit=10");
+    setTask(closed_task);
+
+    const is_working = await api.getRquest(url + "is_working_on=true");
+    setProgress(is_working);
+
+    const open_tasks = await api.getRquest(url + "is_closed=false&limit=10");
+    setOpenTasks(open_tasks);
+  }
+
   const paramers = {
     alteracao: "1",
     site: "2",
-    dev: "3",
-    pende: "4",
+    progress: "3",
+    open: "4",
   };
   const title = {
     alteracao: "Alterações",
     site: "Sites",
-    dev: "Desenvolvimento",
-    pende: "Pendentes",
+    progress: "Desenvolvimento",
+    open: "Pendentes",
   };
 
   return (
@@ -24,10 +49,28 @@ export default function App() {
       <Routes />
       {/* <FormSearch /> */}
       <Container className="tasks-section-dashboard">
-        <CardFilterTask typeTask={paramers.alteracao} title={title.alteracao} />
-        <CardFilterTask typeTask={paramers.site} title={title.site} />
-        <CardFilterTask typeTask={paramers.dev} title={title.dev} />
-        <CardFilterTask typeTask={paramers.pende} title={title.pende} />
+        <CardFilterTask
+          typeTask={paramers.alteracao}
+          title={title.alteracao}
+          filterCard={true}
+          tasks={tasks}
+        />
+        <CardFilterTask
+          typeTask={paramers.site}
+          title={title.site}
+          filterCard={true}
+          tasks={tasks}
+        />
+        <CardFilterTask
+          typeTask={paramers.progress}
+          title={title.progress}
+          tasks={progress}
+        />
+        <CardFilterTask
+          typeTask={paramers.open}
+          title={title.open}
+          tasks={openTasks}
+        />
       </Container>
     </>
   );
--- src/components/CardFilterTask/index.js
+++ src/components/CardFilterTask/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faDisplay,
@@ -8,59 +8,23 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Card, CardGroup, Container } from "react-bootstrap";
 import DropFilterCard from "../DropFilterCard/";
-import Api from "../../services/api";
 import "./style.scss";
 
 export default function CardFilterTask(props) {
-  const [tasks, setTask] = useState([]);
-  const [filterTask, setFilterTask] = useState([]);
-  const [alteration, setAlteration] = useState([]);
   const [sites, setSites] = useState([]);
-  const [progress, setProgress] = useState([]);
-  const [pending, setPending] = useState([]);
+  const [alteration, setAlteration] = useState([]);
+  const [filterTask, setFilterTask] = useState([]);
 
-  // Use fetch api during load page
-  useEffect(() => {
-    async function fetchData() {
-      const api = new Api();
-      // fetch all tasks
-      var responseClosed = await api.getRquest(
-        "tasks?user_id=desenvolvimento&is_closed=true&limit=50"
-      );
-      // fetch tasks in progress
-      var responseProgress = await api.getRquest(
-        "tasks?user_id=desenvolvimento&is_working_on=true"
-      );
-      // fetch pending tasks
-      var responsePending = await api.getRquest(
-        "tasks?user_id=desenvolvimento&is_closed=false"
-      );
-      // Set value of tasks close
-      setTask(responseClosed);
-      // Set value of tasks in progress
-      setProgress(responseProgress);
-      // Set value of tasks pending
-      setPending(responsePending);
-      setSites(0);
-      setAlteration(0);
-      setFilterTask("Hoje");
-    }
-    fetchData();
-  }, []);
 
-  // takes the data and filters each by task type
-  function amountTasks(data, intervalDate, type) {
+  function countTypeTask(data, intervalDate, type) {
     try {
-      // insert date today Ex : 01/01/2020
-      let today = new Date().toLocaleDateString("pt-BR");
       // Count total of interation
       let count = 1;
       // Map data tasks
-      data.map((data) => {
+      data.map((data, key) => {
         // tasks date closed timestamp
         let dateCloseTask = new Date(data.close_date).getTime();
-        //  call the function that add date filter in days
-        let dateFilter = timeTask(today, intervalDate).getTime();
+        let dateFilter = timeTask(intervalDate).getTime();
         // if tasks date close is biggest that date of filter then count +1
         if (dateCloseTask > dateFilter) {
           // paramers alteration
@@ -79,26 +43,26 @@ export default function CardFilterTask(props) {
     }
   }
 
-  function timeTask(date, intervalDate) {
+  function timeTask(intervalDate) {
     try {
-      var date = new Date();
-      date.setDate(date.getDate() - intervalDate);
-      return date;
+      let today = new Date();
+      today.setDate(today.getDate() - intervalDate);
+      return today;
     } catch (err) {
       console.log(err);
     }
   }
 
-  function getTypeTask(task, type) {
+  function getTypeTask(data, type) {
     try {
       if (type === "1") {
         const regex = /(.Alt*)/;
-        let result = regex.test(task);
+        let result = regex.test(data);
         if (result) return 1;
       }
       if (type === "2") {
         const regex = /(.Sit*)/;
-        let result = regex.test(task);
+        let result = regex.test(data);
         if (result) return 2;
       }
     } catch (err) {
@@ -113,19 +77,19 @@ export default function CardFilterTask(props) {
           <Card.Text as="h5">
             {props.title} <span>| {filterTask}</span>
           </Card.Text>
-          {props.typeTask !== "3" && props.typeTask !== "4" && (
+          {props.filterCard && (
             <DropFilterCard
               amountDay={() => {
-                amountTasks(tasks, 1, props.typeTask);
+                countTypeTask(props.tasks, 1, props.typeTask);
                 setFilterTask("Hoje");
               }}
               amountWeek={() => {
-                amountTasks(tasks, 7, props.typeTask);
+                countTypeTask(props.tasks, 7, props.typeTask);
                 setFilterTask("Semana");
               }}
               amountMonth={() => {
-                amountTasks(tasks, 30, props.typeTask);
-                setFilterTask("Mes");
+                countTypeTask(props.tasks, 30, props.typeTask);
+                setFilterTask("Mês");
               }}
             />
           )}
@@ -138,8 +102,8 @@ export default function CardFilterTask(props) {
           <Container>
             {props.typeTask === "1" && <h6>{alteration}</h6>}
             {props.typeTask === "2" && <h6>{sites}</h6>}
-            {props.typeTask === "3" && <h6>{progress.length}</h6>}
-            {props.typeTask === "4" && <h6>{pending.length}</h6>}
+            {props.typeTask === "3" && <h6>{props.tasks.length}</h6>}
+            {props.typeTask === "4" && <h6>{props.tasks.length}</h6>}
           </Container>
         </Container>
       </Card.Body>
